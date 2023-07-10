6029151266cdc607ad992ab61a5b1dc7d7a9ec25
ranunclulus <morion002@gmail.com>
2023-07-11 08:26:11 +0900

    feat: nodejs file system

--- server/main.js
+++ server/main.js
@@ -4,6 +4,7 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
+    var title = queryData.id;
     if(_url == '/'){
       _url = '/index.html';
     }
@@ -12,8 +13,26 @@ var app = http.createServer(function(request,response){
       response.end();
       return;
     }
-    var template = `${queryData.id}`;
+    var template = `<!doctype html>
+<html>
+<head>
+  <title>WEB1 - Welcome</title>
+  <meta charset="utf-8">
+</head>
+<body>
+  <h1><a href="/">WEB</a></h1>
+  <ol>
+    <li><a href="/?id=HTML">HTML</a></li>
+    <li><a href="/?id=CSS">CSS</a></li>
+    <li><a href="/?id=JavaScript">JavaScript</a></li>
+  </ol>
+   <h2>${title}</h2>
+  <p>
+    ${title} (/ˈdʒɑːvəˌskrɪpt/[6]), often abbreviated as JS, is a high-level, dynamic, weakly typed, prototype-based, multi-paradigm, and interpreted programming language. Alongside HTML and CSS, JavaScript is one of the three core technologies of World Wide Web content production. It is used to make webpages interactive and provide online programs, including video games. The majority of websites employ it, and all modern web browsers support it without the need for plug-ins by means of a built-in JavaScript engine. Each of the many JavaScript engines represent a different implementation of JavaScript, all based on the ECMAScript specification, with some engines not supporting the spec fully, and with many engines supporting additional features beyond ECMA.
+  </p>
+</body>
+</html>`;
     response.writeHead(200);
-    response.end(queryData.id);
+    response.end(template);
 });
 app.listen(3000);
