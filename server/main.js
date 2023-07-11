c326e85c732ac074592452a425bfc0f231c879f5
ranunclulus <morion002@gmail.com>
2023-07-11 17:28:39 +0900

    feat: fs readdir

--- server/main.js
+++ server/main.js
@@ -4,35 +4,70 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    if(_url == '/'){
-      _url = '/index.html';
+    var pathname = url.parse(_url, true).pathname;
+
+    if(pathname === '/') {
+        if(queryData.id === undefined) {
+            fs.readdir('./data', function (error, filelist) {
+                console.log(filelist);
+                var title = "Welcome";
+                var description = "description";
+
+                var list = '<ul>';
+                var i = 0;
+                while(i < filelist.length) {
+                    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+                    i = i + 1;
+                }
+                list = list + '</ul>';
+
+                var template = `<!doctype html>
+                                <html>
+                                <head>
+                                  <title>WEB1 - Welcome</title>
+                                  <meta charset="utf-8">
+                                </head>
+                                <body>
+                                  <h1><a href="/">WEB</a></h1>
+                                  ${list}
+                                   <h2>${title}</h2>
+                                  <p>
+                                  ${description}
+                                  </p>
+                                </body>
+                                </html>`;
+                response.writeHead(200);
+                response.end(template);
+            });
+        } else {
+            var title = queryData.id;
+            fs.readFile(`data/${title}`, 'utf8', function (err, description) {
+                var template = `<!doctype html>
+                                    <html>
+                                    <head>
+                                      <title>WEB1 - Welcome</title>
+                                      <meta charset="utf-8">
+                                    </head>
+                                    <body>
+                                      <h1><a href="/">WEB</a></h1>
+                                      <ol>
+                                        <li><a href="/?id=HTML">HTML</a></li>
+                                        <li><a href="/?id=CSS">CSS</a></li>
+                                        <li><a href="/?id=JavaScript">JavaScript</a></li>
+                                      </ol>
+                                       <h2>${title}</h2>
+                                      <p>
+                                      ${description}
+                                      </p>
+                                    </body>
+                                    </html>`;
+                response.writeHead(200);
+                response.end(template);
+            });
+        }
+    } else {
+        response.writeHead(404);
+        response.end("Not Found");
     }
-    if(_url == '/favicon.ico'){
-      response.writeHead(404);
-      response.end();
-      return;
-    }
-    var template = `<!doctype html>
-<html>
-<head>
-  <title>WEB1 - Welcome</title>
-  <meta charset="utf-8">
-</head>
-<body>
-  <h1><a href="/">WEB</a></h1>
-  <ol>
-    <li><a href="/?id=HTML">HTML</a></li>
-    <li><a href="/?id=CSS">CSS</a></li>
-    <li><a href="/?id=JavaScript">JavaScript</a></li>
-  </ol>
-   <h2>${title}</h2>
-  <p>
-    ${title} (/ˈdʒɑːvəˌskrɪpt/[6]), often abbreviated as JS, is a high-level, dynamic, weakly typed, prototype-based, multi-paradigm, and interpreted programming language. Alongside HTML and CSS, JavaScript is one of the three core technologies of World Wide Web content production. It is used to make webpages interactive and provide online programs, including video games. The majority of websites employ it, and all modern web browsers support it without the need for plug-ins by means of a built-in JavaScript engine. Each of the many JavaScript engines represent a different implementation of JavaScript, all based on the ECMAScript specification, with some engines not supporting the spec fully, and with many engines supporting additional features beyond ECMA.
-  </p>
-</body>
-</html>`;
-    response.writeHead(200);
-    response.end(template);
 });
 app.listen(3000);
